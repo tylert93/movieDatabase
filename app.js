0c1af83cf497830e06f4867b6feddb216948aa7e
Tom Tyler <tom.tyler@live.co.uk>
2020-08-20 19:09:51 +0100

    further update to ES6

--- app.js
+++ app.js
@@ -9,24 +9,24 @@ app.set("view engine", "ejs");
 app.use(express.static("node_modules/@fortawesome/fontawesome-free"));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
+app.get("/", (req, res) => {
     res.render("search");
-})
+});
 
-app.get("/results", function(req, res){
+app.get("/results", (req, res) => {
     let query = req.query.search,
         url = `http://www.omdbapi.com/?apikey=${process.env.OMDB_KEY}&s=${query}`;
     axios.get(url)
-    .then(function(response){
+    .then((response) => {
       let data = response["data"].Search;
       res.render("results", {data:data, query:query})
     })
-    .catch(function(error){
+    .catch((error) => {
         console.log(error);
         res.render("error");
     })
 
-})
+});
 
 app.get("/results/:id", function(req, res){
     let id = req.params.id,
